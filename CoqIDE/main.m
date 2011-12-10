a2a21cdb87a80ebb16b7537e5e6cbd719f0324a7
mzp <mzpppp@gmail.com>
2011-12-10 11:14:11 +0900

    サンプル用のhello worldを追加 refs #868

--- CoqIDE/main.m
+++ CoqIDE/main.m
@@ -3,7 +3,6 @@
 //  CoqIDE
 //
 //  Created by mzp on 11/12/09.
-//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
 //
 
 #include <sys/param.h>
@@ -11,14 +10,56 @@
 #include <unistd.h>
 #import <Foundation/Foundation.h>
 #import "AppDelegate.h"
-
+#include "ocamlrun/byterun/glue.h"
 
 char *pwd = 0;
 char *pwd2 = 0;
+NSBundle                *myloadingBundle = nil;
+char* fullp(NSString*);
+
+char *fullp(NSString *relPath) {
+    char realp[MAXPATHLEN];
+    NSString *fullpath = nil;
+    @autoreleasepool {
+        if( ! [relPath isAbsolutePath] ) {
+            NSMutableArray *imagePathComponents = [NSMutableArray arrayWithArray:[relPath pathComponents]];
+            NSString *file = [imagePathComponents lastObject];
+
+            [imagePathComponents removeLastObject];
+            NSString *imageDirectory = [NSString pathWithComponents:imagePathComponents];
+
+            fullpath = [myloadingBundle pathForResource:file ofType:nil inDirectory:imageDirectory];
+            const char* bundle_String = [fullpath UTF8String];
+            realpath(bundle_String, realp);
+            return strdup(realp);
+        } else {
+            char *rel = strdup([relPath UTF8String]);
+            return rel;
+        }
+    }
+}
+
+static void run() {
+    const char* include = fullp(@"ocaml-3.12.0");
+    const char* prog    = fullp(@"ocamlprog");
+    const char* argv[] = {
+        "ocamlrun",
+        "-I",
+        include,
+        prog,
+        NULL
+    };
+    const int argc = sizeof(argv) / sizeof(argv[0]) - 1;
+    ocaml_main(argc,argv);
+    free((char*)include);
+    free((char*)prog);
+}
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+        myloadingBundle = [NSBundle mainBundle];
+        run();
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
