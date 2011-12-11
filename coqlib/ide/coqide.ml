122b80bf0cb930c88028c6f27150e824a06fcbdb
mzp <mzpppp@gmail.com>
2011-12-11 11:23:43 +0900

    coq.mlの使い方サンプルを作った refs #870

--- coqlib/ide/coqide.ml
+++ coqlib/ide/coqide.ml
@@ -1,2 +1,77 @@
+let (@@) f g = f g
+
+type ide_info = unit
+let cmd_stack : (ide_info * Coq.reset_info) Stack.t =
+  Stack.create ()
+
+let mode () =
+  match Decl_mode.get_current_mode () with
+      Decl_mode.Mode_none -> "none"
+    | Decl_mode.Mode_tactic -> "tactic"
+    | Decl_mode.Mode_proof -> "proof"
+
+let string_of_assoc xs =
+  List.map (fun (name, value) -> Printf.sprintf "%s: %s" name value) xs
+
+let goal (hyps, concl) =
+  List.iter
+    (fun ((_,_,_,(s,_))) -> Printf.printf "[hyp] %s\n" s) hyps;
+  let (_,_,_,s) = concl in
+    Printf.printf "[goal] %s" s
+
+let show_goal () =
+  try
+    if mode() <> "none" then
+      goal @@ Coq.get_current_pm_goal ()
+  with Not_found ->
+    match Decl_mode.get_end_command (Pfedit.get_pftreestate ()) with
+        Some endc ->
+          print_endline ("Subproof completed, now type "^endc^".")
+      | None ->
+          print_endline "Proof completed."
+
+let eval s =
+  match s with
+    | "$undo" ->
+        let _,coq =
+          Stack.pop cmd_stack in
+          Coq.rewind [coq] cmd_stack
+    | "$undo2" ->
+        let _,x =
+          Stack.pop cmd_stack in
+        let _,y =
+          Stack.pop cmd_stack in
+          Coq.rewind [x;y] cmd_stack
+    | _ ->
+        let reset_info =
+          Coq.interp true s in
+          Coq.push_phrase cmd_stack reset_info ()
+
+let rep s =
+  print_endline s;
+  print_endline "=== mode";
+  print_endline @@ mode ();
+  print_endline "=== interp";
+  eval s;
+  print_endline "=== goal:";
+  print_endline @@ mode ();
+  show_goal ();
+  print_endline "------------------------------\n"
+
 let start () =
-  print_endline (Coq.version())
+  ignore @@ Coq.init ();
+  List.iter rep [
+    "Definition x := 42.";
+    "$undo";
+    "Definition x := 41.";
+    "Definition y := 41.";
+    "$undo2";
+    "Definition x := 40.";
+    "Goal forall A, A -> A.";
+    "Proof.";
+    "intros.";
+    "assumption.";
+    "Qed."
+  ];
+  print_endline @@ mode ()
+
